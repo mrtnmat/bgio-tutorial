3c871ca08c0afa48519b1f9a6470d4eec9251234
Matteo Martini <mrtn.matteo@proton.me>
2024-07-19 15:15:44 +0200

    added tailwind dep

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import { Client } from 'boardgame.io/react';
+import { Local } from 'boardgame.io/multiplayer';
 import { TicTacToe } from './Game';
 import { TicTacToeBoard } from './Board';
 
-const App = Client({
+const TicTacToeClient = Client({
   game: TicTacToe,
   board: TicTacToeBoard,
+  multiplayer: Local(),
 });
+
+const App = () => (
+  <div>
+    <div class="w-full bg-red-500 h-4"></div>
+    <TicTacToeClient playerID="0" />
+    <TicTacToeClient playerID="1" />
+  </div>
+)
 
 
 export default App;
